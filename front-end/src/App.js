28628a9bddde443b45ed4907296605df27e11848
JaninduTW <149558628+JaninduTW@users.noreply.github.com>
2024-09-01 00:19:46 +0530

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Analytics from "./Components/Analytics";
 import Newsletter from "./Components/Newsletter";
+import Cards from "./Components/Cards";
+import Menu from "./Components/Menu";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Hero/>
       <Analytics/>
       <Newsletter/>
+      <Cards/>
+      <Menu/>
     </div>
   );
 }
