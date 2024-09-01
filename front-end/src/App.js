6c6abeb556cb632bc039f2afc8703e9261503051
JaninduTW <149558628+JaninduTW@users.noreply.github.com>
2024-09-01 19:07:42 +0530

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,20 +1,34 @@
 import React from "react";
-import Navbar from "./Components/Navbar";
-import Hero from "./Components/Hero";
-import Analytics from "./Components/Analytics";
-import Newsletter from "./Components/Newsletter";
-import Cards from "./Components/Cards";
-import Menu from "./Components/Menu";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import LoginForm from "./Pages/LoginForm";
+import Menu from "./Pages/Menu";
+import Contact from "./Pages/Contact";
+import About from "./Pages/About";
+import Gallery from "./Pages/Gallery";
+import Reservation from "./Pages/Reservation";
+import Order from "./Pages/Order";
+import Registration from "./Pages/Registration";
 
 function App() {
   return (
     <div >
-      <Navbar/>
-      <Hero/>
-      <Analytics/>
-      <Newsletter/>
-      <Cards/>
-      <Menu/>
+       <Router>
+      
+        <Routes>
+        <Route exact path='/' element={ <Home/>}     />      
+        <Route exact path='/loginform' element={ <LoginForm/>}     />      
+        <Route exact path='/registraion' element={ <Registration/>}     />      
+        <Route exact path='/menu' element={ <Menu/>}     />      
+        <Route exact path='/reservation' element={ <Reservation/>}     />      
+        <Route exact path='/order' element={ <Order/>}     />      
+        <Route exact path='/contact' element={ <Contact/>}     />      
+        <Route exact path='/about' element={ <About/>}     />      
+        <Route exact path='/gallery' element={ <Gallery/>}   />      
+      </Routes>
+    
+      </Router>
+     
     </div>
   );
 }
