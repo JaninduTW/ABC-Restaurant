b3e73e2be22eb44d2c23e6e26caba8bce9b65efe
JaninduTW <149558628+JaninduTW@users.noreply.github.com>
2024-09-01 19:08:06 +0530

    Update Cards.jsx

--- front-end/src/Components/Cards.jsx
+++ front-end/src/Components/Cards.jsx
@@ -1,42 +1,30 @@
 import React from 'react'
-import User from '../Assests/User.png'
+import offer1 from '../Assests/offer1.png'
+import Offer2 from '../Assests/Offer2.png'
+import Offer3 from '../Assests/Offer3.png'
+
 
 const Cards = () => {
     return (
-        <div className=' w-full py-[10rem] px-4 bg-white' >
-            <div className='max-w-[1240] mx-auto grid md:grid-cols-3 gap-8' >
+        <div className=' w-full py-[3rem] px-4 bg-white' >
+              <div className='text-center' ><p className='text-[#000000] font-bold py-3  text-3xl pb-2 bg-white ' >OFFERS</p></div>
+            <div className='max-w-[1240] mx-auto grid md:grid-cols-3 gap-8 pt-12 ' >
                 <div className='w-full shadow-xl flex flex-col p-4 my-6 rounded-lg hover:scale-105 duration-300 '  >
-                    <img className='w-20 mx-auto mt-[-3rem] bg-white' src={User} alt="User" />
-                    <h2 className='text-2xl font-bold text-center py-8' >Single User</h2>
-                    <p className='text-center text-4xl font-bold' >$149</p>
-                    <div className='text-center font-medium' >
-                        <p className='py-2 border-b mx-8 mt-8'>500GB Storage</p>
-                        <p className='py-2 border-b mx-8 mt-8'>1 Granted User</p>
-                        <p className='py-2 border-b mx-8 mt-8'>Send up to 2GB</p>
-                    </div >
-                    <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 px-6 mx-auto py-3 text-black' >Start trail</button>
+                    <img className='w-300 mx-auto mt-[-3rem] bg-white' src={offer1} alt="User" />
+                   
+                    
+                   
                 </div>
-                <div className='w-full shadow-xl flex flex-col p-4 md:my-0  rounded-lg hover:scale-105 duration-300 '  >
-                    <img className='w-20 mx-auto mt-[-3rem] bg-white' src={User} alt="User" />
-                    <h2 className='text-2xl font-bold text-center py-8' >Single User</h2>
-                    <p className='text-center text-4xl font-bold' >$149</p>
-                    <div className='text-center font-medium' >
-                        <p className='py-2 border-b mx-8 mt-8'>500GB Storage</p>
-                        <p className='py-2 border-b mx-8 mt-8'>1 Granted User</p>
-                        <p className='py-2 border-b mx-8 mt-8'>Send up to 2GB</p>
-                    </div >
-                    <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 px-6 mx-auto py-3 text-black' >Start trail</button>
+                <div className='w-full shadow-xl flex flex-col p-4 my-6  rounded-lg hover:scale-105 duration-300 '  >
+                    <img className='w-300 mx-auto mt-[-3rem] bg-white' src={Offer2} alt="User" />
+                    
+                    
+                  
                 </div>
                 <div className='w-full shadow-xl flex flex-col p-4  my-6 rounded-lg hover:scale-105 duration-300 '  >
-                    <img className='w-20 mx-auto mt-[-3rem] bg-white' src={User} alt="User" />
-                    <h2 className='text-2xl font-bold text-center py-8' >Single User</h2>
-                    <p className='text-center text-4xl font-bold' >$149</p>
-                    <div className='text-center font-medium' >
-                        <p className='py-2 border-b mx-8 mt-8'>500GB Storage</p>
-                        <p className='py-2 border-b mx-8 mt-8'>1 Granted User</p>
-                        <p className='py-2 border-b mx-8 mt-8'>Send up to 2GB</p>
-                    </div >
-                    <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 px-6 mx-auto py-3 text-black' >Start trail</button>
+                    <img className='w-300 mx-auto mt-[-3rem] bg-white' src={Offer3} alt="User" /> 
+                    
+                   
                 </div>
             </div>
         </div>
