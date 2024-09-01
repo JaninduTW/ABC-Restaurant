ce07eccef1ad1163f01837f800ea299978f8572c
JaninduTW <149558628+JaninduTW@users.noreply.github.com>
2024-09-01 19:08:12 +0530

    Update Hero.jsx

--- front-end/src/Components/Hero.jsx
+++ front-end/src/Components/Hero.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import { ReactTyped } from "react-typed";
+import burger from '../Assests/burger.png'
 
 const Hero = () => {
   return (
-    <div>
-        <div className='text-white' >
-           <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ' >
-               <p className='text-[#00df9a] font-bold  p-2 ' >GROWING WITH DATA ANALYTICS</p>
-               <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6 ' >Grow with data.</h1>
-               <div className='flex justify-center items-center ' >
-                 <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4 ' >Fast, flexible financing for </p>
-                 <ReactTyped className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2 ' strings = {['BTB', 'BTC', "SAAS"]} typeSpeed={120} backSpeed={140} loop />
+  
+        <div className='text-white max-w-[1240px] mx-auto grid md:grid-cols-2 mt-[4rem] ' >
+           <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center '> 
+               {/* <p className='text-[#f09c20] font-bold  p-2 ' >GROWING WITH DATA ANALYTICS</p> */}
+               <h1 className='md:text-5xl sm:text-4xl text-3xl font-bold md:py-1 ' >A TASTE YOU'LL REMEMBER.</h1>
+               <div className='flex justify-center items-center '>
+                 <p className='md:text-3xl sm:text-2xl text-xl font-bold py-4 mt-1 ' >Delicious, Tasty Foods From </p> 
+                 <ReactTyped className='md:text-3xl sm:text-3xl text-xl font-bold md:pl-2 pl-2 text-[#f09c20]  mt-2 ' strings = {['ABC', 'RESTAURANT']} typeSpeed={80} backSpeed={80} loop />
                </div>
-               <p className='md:text-2xl text-xl font-bold text-gray-500' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, quidem.</p>
-               <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black ' >Get Started</button>
+               <p className='md:text-2xl text-xl font-bold text-gray-500' >Welcome to ABC Restaurant, where great flavors and a cozy atmosphere create a memorable dining experience. 
+                Discover our diverse menu and enjoy culinary excellence today!</p>
+               <button className='bg-[#5ba835] w-[200px] rounded-md font-large my-6 mx-auto py-3 text-white  font-bold hover:scale-105 duration-300' >Browse Menu</button>
            </div> 
+           <img  className='w-[600px] mx-auto mt-[4rem] hover:scale-105 duration-300 ' src={burger} alt="burger" /> 
         </div>
-    </div>
+        
   )
 }
 
