8adbfffa591416bd093f555ba8656dc6c6c1f6d9
JaninduTW <149558628+JaninduTW@users.noreply.github.com>
2024-09-01 20:26:50 +0530

    Update Hero.jsx

--- front-end/src/Components/Hero.jsx
+++ front-end/src/Components/Hero.jsx
@@ -5,10 +5,10 @@ import burger from '../Assests/burger.png'
 const Hero = () => {
   return (
   
-        <div className='text-white max-w-[1240px] mx-auto grid md:grid-cols-2 mt-[4rem] ' >
-           <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center '> 
+        <div className='text-white max-w-[1240px] mx-auto grid md:grid-cols-2 mt-[7rem] ' >  
+           <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center '>  
                {/* <p className='text-[#f09c20] font-bold  p-2 ' >GROWING WITH DATA ANALYTICS</p> */}
-               <h1 className='md:text-5xl sm:text-4xl text-3xl font-bold md:py-1 ' >A TASTE YOU'LL REMEMBER.</h1>
+               <h1 className='md:text-5xl sm:text-4xl text-3xl font-bold md:py-1 ' >A TASTE YOU'LL REMEMBER.</h1> 
                <div className='flex justify-center items-center '>
                  <p className='md:text-3xl sm:text-2xl text-xl font-bold py-4 mt-1 ' >Delicious, Tasty Foods From </p> 
                  <ReactTyped className='md:text-3xl sm:text-3xl text-xl font-bold md:pl-2 pl-2 text-[#f09c20]  mt-2 ' strings = {['ABC', 'RESTAURANT']} typeSpeed={80} backSpeed={80} loop />
@@ -17,8 +17,8 @@ const Hero = () => {
                 Discover our diverse menu and enjoy culinary excellence today!</p>
                <button className='bg-[#5ba835] w-[200px] rounded-md font-large my-6 mx-auto py-3 text-white  font-bold hover:scale-105 duration-300' >Browse Menu</button>
            </div> 
-           <img  className='w-[600px] mx-auto mt-[4rem] hover:scale-105 duration-300 ' src={burger} alt="burger" /> 
-        </div>
+           <img  className='w-[500px] mx-auto mt-[5rem] hover:scale-105 duration-300 ' src={burger} alt="burger" /> 
+        </div>   
         
   )
 }
