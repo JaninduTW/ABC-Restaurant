0d263aa15fbf3faec73ab2a50516b489549f212f
JaninduTW <149558628+JaninduTW@users.noreply.github.com>
2024-09-02 18:39:59 +0530

    Update Gallery.jsx

--- front-end/src/Pages/Gallery.jsx
+++ front-end/src/Pages/Gallery.jsx
@@ -1,138 +1,82 @@
-import React from 'react'
-import burger from '../Assests/burger.png'
-import food1 from '../Assests/food1.png'
-import g1 from '../Assests/g1.jpg'
-import g2 from '../Assests/g2.jpg'
-import g3 from '../Assests/g3.png'
-import Navbar from '../Components/Navbar'
-import Footer from '../Components/Footer'
+import React from 'react';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+import g1 from '../Assests/g1.jpg';
+import g2 from '../Assests/g2.jpg';
+import g3 from '../Assests/g3.png';
+
+
+const images = [g1, g2, g3];
 
 const Gallery = () => {
   return (
     <div>
       <Navbar />
 
-      <div className='w-full px-4 bg-white' >
-
-        <div className='w-3/4 pt-[5rem] mx-auto mb-3'>
-          <img src={g1} alt="g1" />
+      <div className='w-full px-4 bg-gray-50 p-3 '>
+        <div className='text-center pt-20'>
+          <h1 className='text-4xl font-bold mb-10'>Gallery</h1> 
         </div>
 
-        <div>
-          <h1 className='p-5 text-center text-2xl font-bold '  >Kitchen</h1>
+        <div className='max-w-[1240px] mx-auto mb-10 '>
+          <div className='mb-12'>
+            <h2 className='text-2xl font-semibold text-center mb-6'>KITCHEN</h2>
+            <div className='grid md:grid-cols-3 gap-8'>
+              {images.map((img, index) => (
+                <div key={index} className='relative'> 
+                  <img
+                    className='w-full h-64 object-cover rounded-lg shadow-lg hover:scale-105 transition-transform duration-300'
+                    src={img}
+                    alt={`Gallery ${index + 1}`}
+                  />
+                  <div className='absolute inset-0 bg-black opacity-0 hover:opacity-40 transition-opacity duration-300 rounded-lg flex items-center justify-center text-white font-bold text-lg'>
+                    {`Image ${index + 1}`}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className='mb-12'>
+            <h2 className='text-2xl font-semibold text-center mb-6'>INTERIOR</h2>
+            <div className='grid md:grid-cols-3 gap-8'>
+              {images.map((img, index) => (
+                <div key={index} className='relative'>
+                  <img
+                    className='w-full h-64 object-cover rounded-lg shadow-lg hover:scale-105 transition-transform duration-300'
+                    src={img}
+                    alt={`Gallery ${index + 1}`}
+                  />
+                  <div className='absolute inset-0 bg-black opacity-0 hover:opacity-40 transition-opacity duration-300 rounded-lg flex items-center justify-center text-white font-bold text-lg'>
+                    {`Image ${index + 1}`}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className='mb-12'>
+            <h2 className='text-2xl font-semibold text-center mb-6'>EXTERIOR</h2>
+            <div className='grid md:grid-cols-3 gap-8'>
+              {images.map((img, index) => (
+                <div key={index} className='relative'>
+                  <img
+                    className='w-full h-64 object-cover rounded-lg shadow-lg hover:scale-105 transition-transform duration-300'
+                    src={img}
+                    alt={`Gallery ${index + 1}`}
+                  />
+                  <div className='absolute inset-0 bg-black opacity-0 hover:opacity-40 transition-opacity duration-300 rounded-lg flex items-center justify-center text-white font-bold text-lg'>
+                    {`Image ${index + 1}`}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-
-        <div className='max-w-[1240] mx-auto grid md:grid-cols-4 gap-8     '>
-
-          <div className='w-full shadow-xl flex flex-col p-4 my-6 rounded-lg hover:scale-105 duration-300  ' >
-            <img className='w-200 mx-auto mt-[-2rem]' src={g2} alt="burger" />
-
-
-          </div>
-
-          <div className='w-full shadow-xl flex flex-col p-4 my-6 rounded-lg hover:scale-105 duration-300' >
-            <img className='w-200 mx-auto mt-[-2rem]' src={g3} alt="burger" />
-
-
-          </div>
-
-          <div className='w-full shadow-xl flex flex-col p-4 my-6 rounded-lg hover:scale-105 duration-300' >
-            <img className='w-200 mx-auto mt-[-2rem]' src={g2} alt="burger" />
-
-
-          </div>
-
-          <div className='w-full shadow-xl flex flex-col p-4 my-6 rounded-lg hover:scale-105 duration-300' >
-
-            <img className='w-200 mx-auto mt-[-2rem]' src={g3} alt="burger" />
-
-          </div>
-          {/* <button className= 'text-white  bg-[#f09c20] w-[200px] rounded-md font-medium  mx-auto md:mx-[650px] py-3 mb-4 hover:scale-105 duration-300' >See More</button> */}
-
-        </div>
-
-
-        <div>
-          <h1 className='p-5 text-center text-2xl font-bold '  >Interior</h1>
-        </div>
-
-        <div className='max-w-[1240] mx-auto grid md:grid-cols-4 gap-8     '>
-
-          <div className='w-full shadow-xl flex flex-col p-4 my-6 rounded-lg hover:scale-105 duration-300  ' >
-            <img className='w-200 mx-auto mt-[-2rem]' src={g2} alt="burger" />
-
-
-          </div>
-
-          <div className='w-full shadow-xl flex flex-col p-4 my-6 rounded-lg hover:scale-105 duration-300' >
-            <img className='w-200 mx-auto mt-[-2rem]' src={g3} alt="burger" />
-
-
-          </div>
-
-          <div className='w-full shadow-xl flex flex-col p-4 my-6 rounded-lg hover:scale-105 duration-300' >
-            <img className='w-200 mx-auto mt-[-2rem]' src={g2} alt="burger" />
-
-
-          </div>
-
-          <div className='w-full shadow-xl flex flex-col p-4 my-6 rounded-lg hover:scale-105 duration-300' >
-
-            <img className='w-200 mx-auto mt-[-2rem]' src={g3} alt="burger" />
-
-          </div>
-          {/* <button className= 'text-white  bg-[#f09c20] w-[200px] rounded-md font-medium  mx-auto md:mx-[650px] py-3 mb-4 hover:scale-105 duration-300' >See More</button> */}
-
-
-
-
-
-
-        </div>
-
-
-
-        <div>
-          <h1 className='p-5 text-center text-2xl font-bold '  >Exterior</h1>
-        </div>
-
-        <div className='max-w-[1240] mx-auto grid md:grid-cols-4 gap-8     '>
-
-          <div className='w-full shadow-xl flex flex-col p-4 my-6 rounded-lg hover:scale-105 duration-300  ' >
-            <img className='w-200 mx-auto mt-[-2rem]' src={g2} alt="burger" />
-
-
-          </div>
-
-          <div className='w-full shadow-xl flex flex-col p-4 my-6 rounded-lg hover:scale-105 duration-300' >
-            <img className='w-200 mx-auto mt-[-2rem]' src={g3} alt="burger" />
-
-
-          </div>
-
-          <div className='w-full shadow-xl flex flex-col p-4 my-6 rounded-lg hover:scale-105 duration-300' >
-            <img className='w-200 mx-auto mt-[-2rem]' src={g2} alt="burger" />
-
-
-          </div>
-
-          <div className='w-full shadow-xl flex flex-col p-4 my-6 rounded-lg hover:scale-105 duration-300' >
-
-            <img className='w-200 mx-auto mt-[-2rem]' src={g3} alt="burger" />
-
-          </div>
-          {/* <button className= 'text-white  bg-[#f09c20] w-[200px] rounded-md font-medium  mx-auto md:mx-[650px] py-3 mb-4 hover:scale-105 duration-300' >See More</button> */}
-
-
-
-
-
-        </div>
-
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
