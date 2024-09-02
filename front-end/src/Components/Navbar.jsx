8458e07945c564e88661c872b74e9bbd7db08136
JaninduTW <149558628+JaninduTW@users.noreply.github.com>
2024-09-02 12:45:08 +0530

    Update Navbar.jsx

--- front-end/src/Components/Navbar.jsx
+++ front-end/src/Components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     }
   return (
     <div className='fixed top-0 left-0 w-full bg-black bg-opacity-70 text-white z-10 h '>
-            <div className='flex justify-between items-center h-15 max-w-[1240px] mx-auto px-4'>
+            <div className='flex justify-between items-center h-15 max-w-[1240px] mx-auto px-4  '>
        <h1 className='w-full text-3xl font-bold text-[#f09c20] m-4 cursor-pointer ' >ABC</h1>
         <ul className='hidden md:flex'>
             <Link className='p-4 cursor-pointer  ' to="/" >Home</Link>  
