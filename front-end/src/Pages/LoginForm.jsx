44e56a903c8541cf8ab05c853f81777b1022adbe
JaninduTW <149558628+JaninduTW@users.noreply.github.com>
2024-09-02 12:45:25 +0530

    Update LoginForm.jsx

--- front-end/src/Pages/LoginForm.jsx
+++ front-end/src/Pages/LoginForm.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 const LoginForm = () => {
   return (
-    <div className='bg-gray-200 h-full '>
-    <div className='flex w-full h-screen justify-center items-center'>   
-    <div className='w-full lg:w-2/5 justify-center items-center bg-white px-10 py-20 rounded-3xl border-2 border-gray-200'>
+    <div className='bg-white h-full '>
+    <div className='flex w-full h-screen justify-center items-center  '>   
+    <div className='w-full lg:w-2/5 justify-center items-center bg-white px-10 py-10  mt-5 rounded-3xl border-2 border-gray-200 shadow '>
       <h1 className='text-5xl font-semibold text-center '>Welcome Back</h1>
       <p className='font-medium text-lg text-gray-500 mt-4 text-center '>Welcome back! Please Enter Your Sign-in details.</p>
       <div className='mt-8'>
@@ -34,7 +34,7 @@ const LoginForm = () => {
           <button className='ml-2 font-medium text-base text-blue-400'>Forgot password</button>
         </div>
         <div className='mt-8 flex flex-col gap-y-4'>
-          <button className='bg-[#f09c20] text-white text-lg font-bold py-3 rounded-xl active:scale-[.98] transition-all hover:scale-[1.01] ease-in-out'>Sign-in</button>
+          <Link className='bg-[#f09c20] text-white text-lg font-bold py-3 rounded-xl active:scale-[.98] transition-all hover:scale-[1.01] ease-in-out  text-center ' to='/' >Sign-in</Link>
           {/* <button className='flex items-center justify-center gap-2 active:scale-[.98] transition-all hover:scale-[1.01] ease-in-out'>
             Sign-in with Google</button> */}
         </div>
