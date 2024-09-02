3b049b77422f80c10109760948a38a388f056479
JaninduTW <149558628+JaninduTW@users.noreply.github.com>
2024-09-02 18:39:43 +0530

    Update Analytics.jsx

--- front-end/src/Components/Analytics.jsx
+++ front-end/src/Components/Analytics.jsx
@@ -10,7 +10,7 @@ const Analytics = () => {
            <img className='w-[350px] mx-auto my-4  hover:scale-105 duration-300 ' src={chef} alt="/" />
             <div className='flex flex-col justify-center' >
                <p className='text-[#f09c20] font-3xl font-bold py-1 ' >OUR SERVICES</p> 
-               <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-1 mb-2' >Manage Data Analytics Centrally</h1>
+               <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-1 mb-2' >Find Out Our Services</h1>
                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
                <div><button className= 'text-white  bg-[#f09c20] w-[200px] rounded-md font-medium my-4 mx-auto md:mr-3 py-3 hover:scale-105 duration-300 ' >Reserve a Table</button>
                <button className= 'text-white  bg-[#5ba834] w-[200px] rounded-md font-medium my-4 mx-auto md:mx-0 py-3 hover:scale-105 duration-300 ' >Order</button>
