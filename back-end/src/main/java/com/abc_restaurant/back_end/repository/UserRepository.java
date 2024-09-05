84012460727614d71cf7b8a939959642e899a8e1
JaninduTW <149558628+JaninduTW@users.noreply.github.com>
2024-09-05 12:03:00 +0530

    Delete some files

--- back-end/src/main/java/com/abc_restaurant/back_end/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.abc_restaurant.back_end.repository;
-
-import com.abc_restaurant.back_end.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
